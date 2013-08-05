5920e0f366e5fe9d7254840c16e3c5e7514e2660
Cameron Desautels <camdez@gmail.com>
2013-08-04 20:07:39 -0500

    word-count: clean up more using frequencies

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -1,14 +1,7 @@
 (ns phrase
   (:require [clojure.string :as str]))
 
-(defn- count-occurrences [coll]
-  (reduce
-   (fn [counts x]
-     (update-in counts [x] (fnil inc 0)))
-   {}
-   coll))
-
 (defn word-count [s]
   (->> (str/split (str/lower-case s) #"\W")
        (remove str/blank?)
-       (count-occurrences)))
+       (frequencies)))
