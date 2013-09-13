01f967c5ab95c68ddcfb44039ac6d6cd6b1e9c68
Cameron Desautels <camdez@gmail.com>
2013-09-13 11:37:07 -0500

    beer-song: simplify format string by modifying argument

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -2,8 +2,9 @@
   (:require [clojure.pprint :refer [cl-format]]))
 
 (defn verse [beer-count]
-  (cl-format nil "~[No more~:;~:*~D~] bottle~:P of beer on the wall, ~:*~[no more~:;~:*~D~] bottle~:P of beer.~%~:*~[Go to the store and buy some more~:;Take ~:*~[~;it~:;one~] down and pass it around~], ~:*~[99~;no more~*~:;~D~] bottle~:P of beer on the wall.~%"
-             beer-count (dec beer-count)))
+  (cl-format nil "~[No more~:;~:*~D~] bottle~:P of beer on the wall, ~:*~[no more~:;~:*~D~] bottle~:P of beer.~%~:*~[Go to the store and buy some more~:;Take ~:*~[~;it~:;one~] down and pass it around~], ~[no more~:;~:*~D~] bottle~:P of beer on the wall.~%"
+             beer-count
+             (if (zero? beer-count) 99 (dec beer-count))))
 
 (defn sing
   ([start] (sing start 0))
