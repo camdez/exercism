ccf066047adf56ed6f586e98b86466ce7b810d82
Cameron Desautels <camdez@gmail.com>
2013-09-13 11:40:29 -0500

    beer-count: introduce explanatory next-beer-count binding

--- clojure/beer-song/beer.clj
+++ clojure/beer-song/beer.clj
@@ -2,9 +2,9 @@
   (:require [clojure.pprint :refer [cl-format]]))
 
 (defn verse [beer-count]
-  (cl-format nil "~[No more~:;~:*~D~] bottle~:P of beer on the wall, ~:*~[no more~:;~:*~D~] bottle~:P of beer.~%~:*~[Go to the store and buy some more~:;Take ~:*~[~;it~:;one~] down and pass it around~], ~[no more~:;~:*~D~] bottle~:P of beer on the wall.~%"
-             beer-count
-             (if (zero? beer-count) 99 (dec beer-count))))
+  (let [next-beer-count (if (zero? beer-count) 99 (dec beer-count))]
+    (cl-format nil "~[No more~:;~:*~D~] bottle~:P of beer on the wall, ~:*~[no more~:;~:*~D~] bottle~:P of beer.~%~:*~[Go to the store and buy some more~:;Take ~:*~[~;it~:;one~] down and pass it around~], ~[no more~:;~:*~D~] bottle~:P of beer on the wall.~%"
+               beer-count next-beer-count)))
 
 (defn sing
   ([start] (sing start 0))
