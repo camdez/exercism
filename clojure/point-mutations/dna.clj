0eb941f664adf9f2bea185e3cd37152d924e1370
Cameron Desautels <camdez@gmail.com>
2013-09-17 10:19:12 -0500

    point-mutations: alternate implementation

--- clojure/point-mutations/dna.clj
+++ clojure/point-mutations/dna.clj
@@ -1,4 +1,7 @@
 (ns dna)
 
 (defn hamming-distance [d1 d2]
-  (count (filter identity (map not= d1 d2))))
+  (reduce (fn [diff-count t]
+            (if t (inc diff-count) diff-count))
+          0
+          (map not= d1 d2)))
